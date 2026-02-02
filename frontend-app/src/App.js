099e3efb71e5db19235f7bdf447ec46a30c8a36e
Sakshi Vijay nandgude <sakshinandgude6@gmail.com>
2026-02-02 17:17:56 +0000

    Fix import path for ProtectedRoute component

--- frontend-app/src/App.js
+++ frontend-app/src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Dashboard from "./pages/Dashboard";
-import ProtectedRoute from "./routes/ProtectedRoutes";
+import ProtectedRoute from "./routes/ProtectedRoute";
 
 function App() {
   return (
