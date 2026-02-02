58f98fbeeddd7973d002cbe2d1b34fbf2807932e
Sakshi Vijay nandgude <sakshinandgude6@gmail.com>
2026-02-02 15:50:13 +0000

    Implement error handling in server start function
    Added error handling for server startup.

--- backend/server.js
+++ backend/server.js
@@ -57,10 +57,15 @@ app.get("/:shortCode" , async (req, res) => {
 });
 
 const startServer = async () => {
+    try{
   await connectDB();
   app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
   });
+    }
+    catch(error){
+        console.error("Failed to start server" , error.message);
+        process.exit(1);
 };
 
 startServer();
